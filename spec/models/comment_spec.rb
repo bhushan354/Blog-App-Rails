bf4a3ffd0e5d85e4fd4d087a6e251a9537fd2d7e
bhushan354 <deshmukhumesh354@gmail.com>
2024-01-12 18:28:27 +0530

    Fix rubocop errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,14 +7,15 @@ RSpec.describe Comment, type: :model do
       expect(association.macro).to eq :belongs_to
     end
 
-    # described_class.reflect_on_association(:user): This line uses reflect_on_association method to get information about the association between the Comment model and the User model.
+    # described_class.reflect_on_association(:user): This line uses reflect_on_association method to
+    # get information about the association between the Comment model and the User model.
 
     it 'belongs to post' do
       association = described_class.reflect_on_association(:post)
-    #   It checks that the association's macro (type) is :belongs_to
+      #   It checks that the association's macro (type) is :belongs_to
       expect(association.macro).to eq :belongs_to
-    #   association's macro means type
-    # In Rails, belongs_to is used to define a one-to-one association
+      #   association's macro means type
+      # In Rails, belongs_to is used to define a one-to-one association
     end
   end
 end
