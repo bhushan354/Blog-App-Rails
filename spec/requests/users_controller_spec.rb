8daec5d1acc4073624a6a27304ba7ad9db0629f1
bhushan354 <deshmukhumesh354@gmail.com>
2024-01-16 17:27:44 +0530

    Fix rubocop errors

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Users Controller specs', type: :request do
   describe 'GET /index' do
-    it "Redirects ti users list pahge" do
+    it 'Redirects ti users list pahge' do
       get '/users/:user_id/posts'
       expect(response).to render_template(:index)
       expect(response.body).to include('All the posts of individual user')
     end
 
     it 'will not show a different template' do
-        get '/users'
-        expect(response).to_not render_template(:show)
-      end
+      get '/users'
+      expect(response).to_not render_template(:show)
+    end
 
     it 'GET /users should return 200' do
       get '/users'
@@ -27,9 +27,9 @@ RSpec.describe 'Users Controller specs', type: :request do
     end
 
     it 'will not show a different template' do
-        get '/users/:user_id'
-        expect(response).to_not render_template(:index)
-      end
+      get '/users/:user_id'
+      expect(response).to_not render_template(:index)
+    end
 
     it 'GET /users/:user_id url should return 200' do
       get '/users/:user_id'
