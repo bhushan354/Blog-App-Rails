a345a9e774101c0c49e4c7075779eb7ba1f244c8
bhushan354 <deshmukhumesh354@gmail.com>
2024-01-18 16:27:09 +0530

    posts controller changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,39 @@
 class PostsController < ApplicationController
+  before_action :set_user
+  before_action :set_post, only: [:show]
+
   def index
-    @user = User.find(params[:user_id])
     @posts = @user.posts
   end
 
   def show
-    @user = User.find(params[:user_id])
-    @post = @user.posts.find(params[:id])
     @comments = @post.comments
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to user_post_path(current_user, @post), notice: 'post was created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_post
+    @post = @user.posts.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
