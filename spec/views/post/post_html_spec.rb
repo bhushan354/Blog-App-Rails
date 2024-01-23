dbca4548a4ed14cfdfecc1228479170324921e25
Umair Muqeem <umairmuqeem@gmail.com>
2024-01-23 12:12:28 +0500

    Update post_html_spec.rb

--- spec/views/post/post_html_spec.rb
+++ spec/views/post/post_html_spec.rb
@@ -20,3 +20,15 @@ RSpec.describe 'Post show page', type: :system do
     visit user_post_path(@user, @post)
     expect(page).to have_content 'Tom'
   end
+    it 'I can see how many comments it has.' do
+    visit user_post_path(@user, @post)
+    expect(page).to have_content 'Comments: 1'
+  end
+  it 'I can see how many likes it has.' do
+    visit user_post_path(@user, @post)
+    expect(page).to have_content 'Likes: 0'
+  end
+  it 'I can see the post body.' do
+    visit user_post_path(@user, @post)
+    expect(page).to have_content 'This is my first post'
+  end
