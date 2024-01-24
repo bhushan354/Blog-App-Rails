632588f93c12612350756eb43a970ca9870ad774
bhushan354 <deshmukhumesh354@gmail.com>
2024-01-24 11:54:00 +0530

    Revert commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   # dependent: :destroy = ensuring that all associated posts comments and likes are deleted when the user is deleted
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, dependent: :destroy
