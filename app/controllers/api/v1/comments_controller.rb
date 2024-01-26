6b3d1d17521151fed7fc1e21baf3e7e1fe685fea
bhushan354 <deshmukhumesh354@gmail.com>
2024-01-26 18:58:19 +0530

    Comments succesfully displayed at api route

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,4 +1,41 @@
 class Api::V1::CommentsController < ApplicationController
+
+    def index
+        set_user
+        set_post
+        set_comments
+
+
+        comments_data = @comments.map do |comment|
+            {
+                id: comment.id,
+                text: comment.text,
+                author_name: comment.user.name
+            }
+        end
+
+        pretty_json = JSON.pretty_generate(comments_data)
+        render json: pretty_json, status: :ok
+    end
+
+      # http://api/v1/users/:user_id/posts/:post_id/comments
+  # e.g: http://localhost:3000/api/v1/users/1/posts/1/comments
+
+    def create
+        set_user
+        set_post
+
+        @comment = @post.comments.build(comment_params.merge(user: current_user))
+
+        if @comment.save
+            render json: @comment.as_json(include: :user), status: :created
+        else
+            render json: { error: @comment.errors.full_messages}, status: :unprocessable_entity
+
+        end
+    end
+    
+
   private
 
   def set_user
